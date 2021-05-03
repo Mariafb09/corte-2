377d7927cd3db8095fe0f948a91a60e726e49009
Mariafb09 <79957055+Mariafb09@users.noreply.github.com>
2021-05-03 16:57:29 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// JavaScript source code
+// Activador de jQuery - Funci�n principal de jQuery
+$(document).ready(function(){
+
+
+    // Activaci�n de transici�n del men� principal y del men� m�vil
+    $("aside#menu-hamburguesa nav a, header article nav a").bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000,'easeOutExpo');
+        event.preventDefault();
+    });
+
+    // Funciones de Abrir y Cerrar el Men� Responsive
+    $('a#abrir-menu').click(function(){
+        $('aside#menu-hamburguesa').animate({
+            right: 0
+        },300,'easeOutExpo');
+
+        $('a#abrir-menu').hide();
+        $('a#cerrar-menu').show();
+
+        event.preventDefault();
+    });
+
+    $('a#cerrar-menu, aside#menu-hamburguesa nav a').click(function(){
+        $('aside#menu-hamburguesa').animate({
+            right: -290
+        },300,'easeOutExpo');
+        
+        $('a#abrir-menu').show();
+        $('a#cerrar-menu').hide();
+
+        event.preventDefault();
+    });
+    
+});
